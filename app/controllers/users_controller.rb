5965dd66aa0ef99b167965174da9b7ef71f92f8d
elliott-king <8884571+elliott-king@users.noreply.github.com>
2020-06-26 12:52:42 -0400

    Handler to create new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+  def create
+    user = User.find_by(email: params[:email])
+    if user 
+      render json: {error: "email already taken"}
+    else
+      render json: User.create(user_params)
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :first_name, :last_name)
+  end
 end
