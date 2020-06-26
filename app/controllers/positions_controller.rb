6faf6f2303ecb36aa4c8d7d84263781e9f76d036
elliott-king <8884571+elliott-king@users.noreply.github.com>
2020-06-26 10:47:37 -0400

    Positions backend

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -1,2 +1,14 @@
 class PositionsController < ApplicationController
+  def index
+    render json: Position.all, except: [:description]
+  end
+
+  def show
+    position = Position.find_by(id: params[:id])
+    if position
+      render json: position
+    else
+      raise ActionController::RoutingError.new('Position not Found')
+    end
+  end
 end
