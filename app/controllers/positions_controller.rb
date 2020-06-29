ff81e41f0421548c8ca7b2b1ee0a975c83efe458
elliott-king <8884571+elliott-king@users.noreply.github.com>
2020-06-29 14:32:58 -0400

    Include Company when passing Position

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -1,12 +1,12 @@
 class PositionsController < ApplicationController
   def index
-    render json: Position.all, except: [:description]
+    render json: Position.all, except: [:description], include: [:company => {:only => [:name, :company_logo, :id]}]
   end
 
   def show
     position = Position.find_by(id: params[:id])
     if position
-      render json: position
+      render json: position, include: [:company => {:only => [:name, :company_logo, :id]}]
     else
       raise ActionController::RoutingError.new('Position not Found')
     end
