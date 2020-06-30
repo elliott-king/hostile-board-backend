5141692e64ee306fa4aaf2701223ed2a470418dc
elliott-king <8884571+elliott-king@users.noreply.github.com>
2020-06-30 12:30:11 -0400

    Message create method

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,4 +3,18 @@ class MessagesController < ApplicationController
     message = Message.find(params[:id])
     render json: message, include: [:company, :user, :position => {:only => [:id, :title]}]
   end
+
+  def create
+    application = Application.find(params[:application_id])
+    user = User.find(params[:user_id])
+    position = application.position
+    company = position.company
+    m = Message.create!(
+      user: user,
+      position: position,
+      company: company,
+      content: params[:content],
+    )
+    render json: m, include: [:company, :user, :position => {:only => [:id, :title]}]
+  end
 end
