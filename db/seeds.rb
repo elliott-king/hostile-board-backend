140abc88b15dd0d7f0a0d1b27ebe98ac8a496498
elliott-king <8884571+elliott-king@users.noreply.github.com>
2020-07-03 14:10:23 -0400

    Make Seeds more professional

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,16 +7,17 @@ Position.destroy_all
 Company.destroy_all
 User.destroy_all
 
-u = User.create(first_name: 'elliott', last_name: 'king', email: 'ek@fi.com')
+u = User.create(first_name: 'Elliott', last_name: 'King', email: 'ek@fi.com',
+                resume: "{\"name\": \"Elliott King\", \"email\": \"elliottking93@gmail.com\", \"mobile_number\": \"716-650-8158\", \"skills\": [\"Ruby\", \"Testing\", \"Access\", \"Engineering\", \"Html\", \"Javascript\", \"Python\", \"Java\", \"Aws\", \"C++\", \"Github\", \"Js\", \"Researching\", \"Tensorflow\", \"Agile\", \"Technical\", \"Mathematics\", \"Analysis\", \"Sql\"], \"college_name\": null, \"degree\": null, \"designation\": [\"SOFTWARE DEVELOPER\"], \"experience\": [\"Google\", \"Software Engineering Intern\", \"Backend for the new Google Sites\", \"\\u25cf Overhauled debug access to comply with GDPR\", \"\\u25cf Implemented full integration testing to reduce technical debt\", \"\\u25cf Dogfooded new library and worked with team to roll out\", \"- Java\", \"Batch analysis of TensorFlow pipelines\", \"- Python\", \"\\u25cf Batching framework to reduce 80% of reused code in machine learning pipelines\", \"\\u25cf Built seamless integration with Google file services\"], \"company_names\": null, \"no_of_pages\": 1, \"total_experience\": 0.0}")
 
 def seed_positions(data)
   data.each do |position|
     company = Company.find_by(name: position["company"])
     if !company
       company_user = User.create!(
-        first_name: 'admin',
-        last_name: position['company'],
-        email: 'admin@' + position['company'] + '.com',
+        first_name: 'Admin',
+        last_name: position['company'].delete(' '),
+        email: 'admin@' + position['company'].downcase.delete(' ') + '.com',
         is_company: true,
       )
       company = Company.create!(
@@ -62,17 +63,32 @@ loop do
   end
 end
 
-m = Message.create!(
+Application.create!(
   user: u, 
-  company: Company.last, 
-  content: "This is a sample message from me to thee",
   position: Company.last.positions.first,
-)
-a = Application.create!(
-  user: u, 
-  position: Position.last, 
   skills: ["Ruby", "Rails"], 
   job_history: "something, somewhere", 
   projects: "somewhat, sometime", 
   written_introduction: "I am broke"
+)
+Message.create!(
+  user: u, 
+  company: Company.last, 
+  content: "This is a sample message from a company to thee",
+  position: Company.last.positions.first,
+)
+
+Application.create!(
+  user: u, 
+  position: Company.first.positions.first,
+  skills: ["Ruby", "Rails"], 
+  job_history: "something, somewhere", 
+  projects: "somewhat, sometime", 
+  written_introduction: "I am broke"
+)
+Message.create!(
+  user: u, 
+  company: Company.first, 
+  content: "You are just not a good fit at this time",
+  position: Company.first.positions.first,
 )
