b102c4a9be94e4754f8c5eaee3a4b1d6bd30ca33
elliott-king <8884571+elliott-king@users.noreply.github.com>
2020-08-29 09:22:07 -0400

    Remove text resume from seedfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,7 @@ Position.destroy_all
 Company.destroy_all
 User.destroy_all
 
-u = User.create(first_name: 'Elliott', last_name: 'King', email: 'ek@fi.com',
-                resume: "{\"name\": \"Elliott King\", \"email\": \"elliottking93@gmail.com\", \"mobile_number\": \"716-650-8158\", \"skills\": [\"Ruby\", \"Testing\", \"Access\", \"Engineering\", \"Html\", \"Javascript\", \"Python\", \"Java\", \"Aws\", \"C++\", \"Github\", \"Js\", \"Researching\", \"Tensorflow\", \"Agile\", \"Technical\", \"Mathematics\", \"Analysis\", \"Sql\"], \"college_name\": null, \"degree\": null, \"designation\": [\"SOFTWARE DEVELOPER\"], \"experience\": [\"Google\", \"Software Engineering Intern\", \"Backend for the new Google Sites\", \"\\u25cf Overhauled debug access to comply with GDPR\", \"\\u25cf Implemented full integration testing to reduce technical debt\", \"\\u25cf Dogfooded new library and worked with team to roll out\", \"- Java\", \"Batch analysis of TensorFlow pipelines\", \"- Python\", \"\\u25cf Batching framework to reduce 80% of reused code in machine learning pipelines\", \"\\u25cf Built seamless integration with Google file services\"], \"company_names\": null, \"no_of_pages\": 1, \"total_experience\": 0.0}")
+u = User.create(first_name: 'Elliott', last_name: 'King', email: 'ek@fi.com')
 
 def seed_positions(data)
   data.each do |position|
@@ -38,7 +37,7 @@ def seed_positions(data)
   end
 end
 
-def self.api_call(page)
+def api_call(page)
   first_fifty = "https://jobs.github.com/positions.json"
   next_page = first_fifty + "?page=" + page.to_s
   
